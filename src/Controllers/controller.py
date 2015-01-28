e67ba357747e9375cb5f9ecf370347aba0365ed1
Hicham <hicham.azimani@live.fr>
2015-01-28 10:06:17 +0100

    mineur cham

---
+++ src/Controllers/controller.py
@@ -0,0 +1,19 @@
+
+
+"""\brief
+	Moteur class
+
+    The detailed description of the class would appear right here.
+"""
+
+class Controller:
+    def __init__(self):
+        pass
+
+
+def testController():
+    pass
+
+
+if __name__=='__main__':
+    testController()
--- src/Controllers/guiController.py
+++
@@ -1,8 +0,0 @@
-"""\brief 
-	Moteur class
-
-    The detailed description of the class would appear right here.
-"""
-
-def fct():
-"""The constructor."""
